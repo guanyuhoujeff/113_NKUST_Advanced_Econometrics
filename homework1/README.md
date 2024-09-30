cca1f17c173e2ba8d5119b8ff5e4200a05bfa24e
jeff7522553 <jeff7522553>
2024-09-30 21:09:15 +0800

    hw2

---
+++ homework1/README.md
@@ -0,0 +1,21 @@
+# 高等計量經濟專題 - 113學年度上學期 (國立高雄科技大學)
+
+此資料夾為作業1
+
+## Google Colab Demo
+
+HW1 : 
+[檔案連結](https://github.com/guanyuhoujeff/113_NKUST_Advanced_Econometrics/tree/main/homework1) [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/guanyuhoujeff/113_NKUST_Advanced_Econometrics/blob/main/homework1/HW1.ipynb)
+
+## 在本地端運行
+
+若想在本地端執行本系統，請依照以下步驟：
+
+```bash
+conda create -n ecm python=3.11 -y
+conda activate ecm
+conda install -c conda-forge jupyter -y
+
+#啟動jupyter
+conda activate ecm && jupyter notebook
+```
---
+++ homework2/HW2.py
@@ -0,0 +1,43 @@
+import yfinance as yf
+from scipy.stats import norm
+import numpy as np
+
+# 1: data=[2 5 7 9 10], find E(data) and Var(data)
+data = np.array([2, 5, 7, 9, 10])
+
+# Calculate mean and variance
+mean_data = np.mean(data)
+var_data = np.var(data)
+
+print("1: data=[2 5 7 9 10], find E(data) and Var(data)")
+print()
+print("Mean of data:", mean_data)
+print("Variance of data:", var_data)
+print("==================================================================")
+
+
+
+# 2. Download TAIEX stock index daily prices and compute returns¶
+ticker = "^TWII"  # TAIEX stock index ticker
+print("start download TWII data : ")
+taiex = yf.download(ticker, period="1y")  # Download data for 1 year
+taiex['Returns'] = taiex['Adj Close'].pct_change().dropna()
+# Define RR (Returns)
+RR = taiex['Returns'].dropna()
+
+# (1)Compute m=E(RR) and v=Var(RR), assuming normal distribution.
+mean_RR = RR.mean()
+var_RR = RR.var()
+print()
+print()
+print("2.1  Compute m=E(RR) and v=Var(RR), assuming normal distribution.")
+print("Mean of RR:", mean_RR)
+print("Variance of RR:", var_RR)
+print("==================================================================")
+print()
+print()
+# (2) Compute the probability that RR > 0 assuming normal distribution
+prob = 1 - norm.cdf(0, loc=mean_RR, scale=np.sqrt(var_RR))
+print("2.2  Compute the probability that RR > 0 assuming normal distribution")
+print("Probability that RR > 0:", prob)
+print("==================================================================")
